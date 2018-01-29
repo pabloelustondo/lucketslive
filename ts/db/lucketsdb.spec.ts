7fa083a250c426c55ad4de2b6fc7897399b6985a
Pablo Elustondo <pablo.elustondo@soti.net>
2018-01-28 19:18:23 -0500

    can delete, insert and read data from mongo

--- ts/db/lucketsdb.spec.ts
+++ ts/db/lucketsdb.spec.ts
@@ -1,21 +1,64 @@
-import { expect } from 'chai';
+import { expect, assert } from 'chai';
 import {LktLucketDB} from './lucketsdb';
 import {LktTestLuckets} from '../testdata/testluckets'
 
 describe("Lucket DB", () => {
+
+    let dbconfig = "mongodb://localhost:27017";
+    let userkey = "test";
+    let lucketDB;
+
         it("it can be created", () => {
-            let lucketDB = new LktLucketDB();
+            lucketDB = new LktLucketDB(dbconfig, userkey);
         });
 
+    it("drops database for user if exists, or says ns not found", (done) => {
 
-    it("it connects to db", (done) => {
-        let lucketDB = new LktLucketDB();
-        lucketDB.connect().then( (db) => {
-            expect(db).to.not.be.undefined;
+        lucketDB.deleteAllLuckets().then( (luckets) => {
+            expect(luckets).to.not.be.undefined;
+            done()
+        }).catch((err)=> {
+            expect(err).to.not.be.undefined;
+            expect(err.message).to.be.equal("ns not found");
             done()
         })
     });
 
 
+    it("gets the existing luckets (empty list) for the user", (done) => {
+
+        lucketDB.getLuckets().then( (luckets:any[]) => {
+            expect(luckets).to.not.be.undefined;
+            expect(luckets.length).to.be.equal(0);
+            done()
+        }).catch((err)=> {
+            assert.fail(err,undefined,"cannot get luckets");
+            done()
+        })
+    });
+
+    it("insert luckets for the user", (done) => {
+
+        lucketDB.insertLuckets().then( (luckets:{insertedCount}) => {
+            expect(luckets).to.not.be.undefined;
+            expect(luckets.insertedCount).to.be.equal(1);
+            done()
+        }).catch((err)=> {
+            assert.fail(err,undefined,"cannot get luckets");
+            done()
+        })
+    });
+
+    it("gets the existing luckets (1 lucket) for the user", (done) => {
+
+        lucketDB.getLuckets().then( (luckets:any[]) => {
+            expect(luckets).to.not.be.undefined;
+            expect(luckets.length).to.be.equal(1);
+            done()
+        }).catch((err)=> {
+            assert.fail(err,undefined,"cannot get luckets");
+            done()
+        })
+    });
 
 });
--- ts/db/lucketsdb.ts
+++ ts/db/lucketsdb.ts
@@ -6,29 +6,69 @@ import { MongoClient, Db, Server } from "mongodb";
 
 export class LktLucketDB {
 
-    constructor(){
+    private dbconfig = "mongodb://localhost:27017";
+    private userkey = "test";
+
+    constructor(dbconfig, userkey) {
+        if (dbconfig) this.dbconfig = dbconfig;
+        if (userkey) this.userkey = userkey;
     }
 
-    public connect() {
-
-        return new Promise((resolve,reject) => {
+    public deleteAllLuckets() {
+        return new Promise((resolve, reject) => {
             let mongoClient = new MongoClient();
-            MongoClient.connect("mongodb://localhost:27017")
+            MongoClient.connect(this.dbconfig)
                 .then(mc => {
-                        mc.db("test").collection("test").find().toArray().then( (d) => {
-
-                            resolve(d);
+                        mc.db("test").collection(this.userkey).drop()
+                            .then((d) => {
+                                resolve(d);
+                            }).catch((err) => {
+                            reject(err);
                         });
-
-                        }
-                    )
-                })
+                    }
+                )
                 .catch(err => {
-                    console.log(err);
+                    reject(err);
                 });
         });
     }
 
+    public getLuckets() {
+        return new Promise((resolve, reject) => {
+            let mongoClient = new MongoClient();
+            MongoClient.connect(this.dbconfig)
+                .then(mc => {
+                        mc.db("test").collection(this.userkey).find().toArray()
+                            .then((d) => {
+                                resolve(d);
+                            }).catch((err) => {
+                            reject(err);
+                        });
+                    }
+                )
+                .catch(err => {
+                    reject(err);
+                });
+        });
+    }
 
+    public insertLuckets() {
+        return new Promise((resolve, reject) => {
+            let mongoClient = new MongoClient();
+            MongoClient.connect(this.dbconfig)
+                .then(mc => {
+                        mc.db("test").collection(this.userkey).insert({'hi':'hi'})
+                            .then((d) => {
+                                resolve(d);
+                            }).catch((err) => {
+                            reject(err);
+                        });
+                    }
+                )
+                .catch(err => {
+                    reject(err);
+                });
+        });
+    }
 
 }
